5b65ac827c3fe22c0a4012513e2f256578e5c2e2
Pratik Kumar <pkumar@tavisca.com>
2017-08-28 11:54:00 +0530

    Made flight type buttons

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-$("#flightsButton").click(function() {
+$("#flightsButton").click(function () {
     $("#flightsButton").addClass("mainActivated");
     $("#hotelsButton").removeClass("mainActivated");
     $("#flightSelection").css("display", "block");
     $("#hotelSelection").css("display", "none");
 });
 
-$("#hotelsButton").click(function() {
+$("#hotelsButton").click(function () {
     $("#hotelsButton").addClass("mainActivated");
     $("#flightsButton").removeClass("mainActivated");
     $("#flightSelection").css("display", "none");
     $("#hotelSelection").css("display", "block");
+});
+
+$("#oneWay").addClass("flightTypeActivated");
+$("#oneWay").click(function () {
+    $("#oneWay").addClass("flightTypeActivated");
+    $("#twoWay").removeClass("flightTypeActivated");
+    $('#oneWay').parent().closest('div').children('input').val('oneWay');
+});
+$("#twoWay").click(function () {
+    $("#twoWay").addClass("flightTypeActivated");
+    $("#oneWay").removeClass("flightTypeActivated");
+    $('#twoWay').parent().closest('div').children('input').val('twoWay');
 });
