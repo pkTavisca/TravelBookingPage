5023f724bc62ed76b856fb4d77ae1fc98bb98b02
Pratik Kumar <pkumar@tavisca.com>
2017-08-28 16:33:56 +0530

    adult and children selection

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
-$("#flightsButton").click(function () {
-    $("#flightsButton").addClass("mainActivated");
-    $("#hotelsButton").removeClass("mainActivated");
-    $("#flightForm").css("display", "block");
-    $("#hotelSelection").css("display", "none");
+//Flights
+var flightsButton = $('#flightsButton');
+var hotelsButton = $('#hotelsButton');
+var flightForm = $('#flightForm');
+var hotelForm = $('#hotelForm');
+var oneWay = $('#oneWay');
+var twoWay = $('#twoWay');
+
+flightsButton.addClass('mainActivated');
+flightsButton.click(function () {
+    flightsButton.addClass('mainActivated');
+    hotelsButton.removeClass('mainActivated');
+    flightForm.show();
+    hotelForm.hide();
 });
 
-$("#hotelsButton").click(function () {
-    $("#hotelsButton").addClass("mainActivated");
-    $("#flightsButton").removeClass("mainActivated");
-    $("#flightForm").css("display", "none");
-    $("#hotelSelection").css("display", "block");
+hotelsButton.click(function () {
+    hotelsButton.addClass('mainActivated');
+    flightsButton.removeClass('mainActivated');
+    flightForm.hide();
+    hotelForm.show();
 });
 
-$("#oneWay").addClass("flightTypeActivated");
+oneWay.addClass('flightTypeActivated');
 $('#flight-returning').hide();
-$("#oneWay").click(function () {
-    $("#oneWay").addClass("flightTypeActivated");
-    $("#twoWay").removeClass("flightTypeActivated");
-    $('#oneWay').parent().closest('div').children('input').val('oneWay');
+oneWay.click(function () {
+    oneWay.addClass('flightTypeActivated');
+    twoWay.removeClass('flightTypeActivated');
+    oneWay.parent().closest('div').children('input').val('oneWay');
     $('#flight-returning').hide();
 });
-$("#twoWay").click(function () {
-    $("#twoWay").addClass("flightTypeActivated");
-    $("#oneWay").removeClass("flightTypeActivated");
-    $('#twoWay').parent().closest('div').children('input').val('twoWay');
+twoWay.click(function () {
+    twoWay.addClass('flightTypeActivated');
+    oneWay.removeClass('flightTypeActivated');
+    twoWay.parent().closest('div').children('input').val('twoWay');
     $('#flight-returning').show();
 });
 
@@ -32,5 +41,25 @@ $('.flightAutoselect').autocomplete({
     source: airports
 });
 
-$(".date").datepicker();
-$('.spinner').spinner();
+$('.date').datepicker();
+
+$('#noOfChildren').change(function (event) {
+    var val = parseInt(event.target.value);
+    $('#children-list').empty();
+    for (var i = 0; i < val; i++) {
+        var $div = $('<div></div>');
+        $('#children-list').append($div);
+        $div.append($('<div class="kid">Child ' + (i + 1) + ' age:</div>'));
+        var $select = $('<select>', {
+            name: "child" + (i + 1)
+        });
+        $div.append($select);
+        $select.append($('<option value="NA">Age</option>'));
+        $select.append($('<option value="<1">Below 1 year</option>'));
+        for (var j = 0; j < 17; j++) {
+            $select.append($('<option value=' + (j + 1) + '>' + (j + 1) + '</option>'));
+        }
+    }
+});
+
+//Hotels
