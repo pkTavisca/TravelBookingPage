e68c0fe4f6b62914a7419ebd4289e9e2abcc9021
Pratik Kumar <pkumar@tavisca.com>
2017-08-28 13:06:16 +0530

    added from to and dates

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 $("#flightsButton").click(function () {
     $("#flightsButton").addClass("mainActivated");
     $("#hotelsButton").removeClass("mainActivated");
-    $("#flightSelection").css("display", "block");
+    $("#flightForm").css("display", "block");
     $("#hotelSelection").css("display", "none");
 });
 
 $("#hotelsButton").click(function () {
     $("#hotelsButton").addClass("mainActivated");
     $("#flightsButton").removeClass("mainActivated");
-    $("#flightSelection").css("display", "none");
+    $("#flightForm").css("display", "none");
     $("#hotelSelection").css("display", "block");
 });
 
 $("#oneWay").addClass("flightTypeActivated");
+$('#flight-returning').hide();
 $("#oneWay").click(function () {
     $("#oneWay").addClass("flightTypeActivated");
     $("#twoWay").removeClass("flightTypeActivated");
     $('#oneWay').parent().closest('div').children('input').val('oneWay');
+    $('#flight-returning').hide();
 });
 $("#twoWay").click(function () {
     $("#twoWay").addClass("flightTypeActivated");
     $("#oneWay").removeClass("flightTypeActivated");
     $('#twoWay').parent().closest('div').children('input').val('twoWay');
+    $('#flight-returning').show();
 });
+
+var airports = ['Pune', 'Mumbai'];
+$('.flightAutoselect').autocomplete({
+    source: airports
+});
+
+$(".date").datepicker();
